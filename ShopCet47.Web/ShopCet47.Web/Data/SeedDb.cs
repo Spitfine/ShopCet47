cda5a580a8a4447da9b2ebe10d554e1ca5d51519
Carlos alves <carlosmspa@netcabo.pt>
2020-09-10 22:58:55 +0100

    Add users

--- ShopCet47.Web/ShopCet47.Web/Data/SeedDb.cs
+++ ShopCet47.Web/ShopCet47.Web/Data/SeedDb.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using ShopCet47.Web.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,12 +10,14 @@ namespace ShopCet47.Web.Data
     public class SeedDb
     {
         private readonly DataContext _context;
+        private readonly UserManager<User> _userManager;
         private Random _random;
 
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
             _random = new Random();
         }
 
@@ -21,24 +25,43 @@ namespace ShopCet47.Web.Data
         {
             await _context.Database.EnsureCreatedAsync();
 
+            var user = await _userManager.FindByEmailAsync("carlosmspa@netcabo.pt");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Carlos",
+                    LastName = "Alves",
+                    Email = "carlosmspa@netcabo.pt",
+                    UserName = "carlosmspa@netcabo.pt"
+
+                };
+                var result = await _userManager.CreateAsync(user,"123456");
+                if(result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
             if (!_context.Products.Any())
             {
-                this.AddProduct("iPhone X");
-                this.AddProduct("Rato Mickey");
-                this.AddProduct("iWatch Series 4");
-                this.AddProduct("Ipad 2");
+                this.AddProduct("iPhone X", user);
+                this.AddProduct("Rato Mickey", user);
+                this.AddProduct("iWatch Series 4", user);
+                this.AddProduct("Ipad 2", user);
                 await _context.SaveChangesAsync();
 
             }
         }
-        private void AddProduct(string name)
+        private void AddProduct(string name, User user)
         {
             _context.Products.Add(new Entities.Product
             {
                 Name = name,
                 Price = _random.Next(1000),
                 IsAvailable = true,
-                Stock = _random.Next(100)
+                Stock = _random.Next(100),
+                User = user
             });
         }
 
